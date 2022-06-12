4ddc58818c70379f55b38317553c67ca8db4fb40
Mykhaylo <msshumko@gmail.com>
2022-06-12 18:42:07 -0400

    Figuring out how to use subprocess.run().

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 import setuptools
 from setuptools.command.develop import develop
 from setuptools.command.install import install
-from subprocess import check_call
+from subprocess import run
 
 # class PreDevelopCommand(develop):
 #     """Pre-installation for development mode."""
@@ -13,7 +13,14 @@ class PreInstallCommand(install):
     """Pre-installation for installation mode."""
     def run(self):
         print('irbempy install script is running.')
-        # check_call("apt-get install this-package".split())
+        _pwd = run("pwd", shell=True, check=True)
+        print(_pwd.stdout)
+        # check_call("cd IRBEM".split(), shell=True)
+        _build = run("cd IRBEM/ && make OS=linux64 ENV=gfortran64 all".split(), shell=True, check=True)
+        print(_build.stdout)
+        print(_build.stderr)
+        # run("cd /IRBEM/ && make OS=linux64 ENV=gfortran64 install".split(), shell=True, check=True)
+        print('irbempy install script is finished.')
         install.run(self)
 
 setuptools.setup(
