fbcfa40ab2cc4da468a52bcd8b21ea128d723f46
Mykhaylo <msshumko@gmail.com>
2022-06-12 18:47:59 -0400

    Figuring out how to run a makefile in setup.py

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ import setuptools
 from setuptools.command.develop import develop
 from setuptools.command.install import install
 from subprocess import run
+import time
 
 # class PreDevelopCommand(develop):
 #     """Pre-installation for development mode."""
@@ -13,10 +14,17 @@ class PreInstallCommand(install):
     """Pre-installation for installation mode."""
     def run(self):
         print('irbempy install script is running.')
-        _pwd = run("pwd", shell=True, check=True)
-        print(_pwd.stdout)
+        # _pwd = run("pwd", shell=True, check=True)
+        # print(_pwd.stdout)
         # check_call("cd IRBEM".split(), shell=True)
         _build = run("cd IRBEM/ && make OS=linux64 ENV=gfortran64 all".split(), shell=True, check=True)
+        start_time = time.time()
+        while _build.stdout is None:
+            print(_build.returncode)
+            if _build.check_returncode():
+                break
+            elif time.time() - start_time > 10:
+                raise TimeoutError('make all IRBEM command timed out after 10 seconds.')
         print(_build.stdout)
         print(_build.stderr)
         # run("cd /IRBEM/ && make OS=linux64 ENV=gfortran64 install".split(), shell=True, check=True)
