c0045ddf788086cee7cb18eece1dde51b2dadf50
Mykhaylo <msshumko@gmail.com>
2022-06-13 18:43:05 -0400

    Trying out the Extention way. It has problems that I guess stem from the gfortran flags.

--- setup.py
+++ setup.py
@@ -1,32 +1,23 @@
-import setuptools
-from setuptools.command.develop import develop
-from setuptools.command.install import install
-import subprocess
-import time
+# import setuptools
+# from setuptools.command.develop import develop
+# from setuptools.command.install import install
+# import subprocess
+# import time
+import pathlib
 
-class PreDevelopCommand(develop):
-    """Pre-installation for development mode."""
-    def run(self):
-        subprocess.Popen("make OS=linux64 ENV=gfortran64 clean".split(), cwd="./IRBEM")
-        print('Building IRBEM-Lib source')
-        subprocess.Popen("make OS=linux64 ENV=gfortran64 all".split(), cwd="./IRBEM")
-        print('Installing IRBEM-Lib source')
-        subprocess.Popen("make OS=linux64 ENV=gfortran64 install".split(), cwd="./IRBEM")
-        install.run(self)
+from numpy.distutils.core import Extension, setup
 
-class PreInstallCommand(install):
-    """Pre-installation for installation mode."""
-    def run(self):
-        subprocess.Popen("make OS=linux64 ENV=gfortran64 clean".split(), cwd="./IRBEM")
-        print('Building IRBEM-Lib source')
-        subprocess.Popen("make OS=linux64 ENV=gfortran64 all".split(), cwd="./IRBEM")
-        print('Installing IRBEM-Lib source')
-        subprocess.Popen("make OS=linux64 ENV=gfortran64 install".split(), cwd="./IRBEM")
-        install.run(self)
+source_dir = pathlib.Path(__file__).parent / 'IRBEM' / 'source'
+sources = [str(path) for path in source_dir.glob('*.f')]
+print(source_dir)
+print(f'{sources=}')
+libirbem = Extension(
+    name='libirbem', sources=sources,
+    extra_f77_compile_args=['-w', '-O2', '-fPIC', '-fno-second-underscore', '-fcheck=all']
+    )
 
-setuptools.setup(
-    cmdclass={
-        'develop': PreDevelopCommand,
-        'install': PreInstallCommand,
-    },
+setup(
+    name = 'irbempy',
+    packages = ['irbempy'],
+    ext_modules = [libirbem],
 )
